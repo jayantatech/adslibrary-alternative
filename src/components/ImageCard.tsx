9c09dfcd7444611f8f54a5ef14b037664cba74d0
Jayanta Biswas <jayantapaikpara@gmail.com>
2024-08-17 13:52:17 +0530

    fix/logo

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -57,35 +57,40 @@ const ImageCard = ({
             <Skeleton className="w-full h-full bg-lightBlue" />
           </div>
         )}
-        {!isImageLoading ? (
-          <>
-            <div className="w-auto h-full flex items-center justify-start gap-1">
-              <div className="w-[48px] max-lg:w-[46px] h-[48px] max-lg:h-[46px] rounded-md">
-                <Image
-                  src={brandLogo}
-                  alt="Brand Logo"
-                  className="w-full h-full rounded-md"
-                />
-              </div>
-              <h3 className="text-[17px] max-lg:text-[16px] font-bold font-OpenSans">
-                {brandName}
-              </h3>
-            </div>
-            <div
-              className={`w-[34px] max-lg:w-[30px] h-[34px] max-lg:h-[30px] bg-transparent rounded-[4px] border-slate-300 hover:border-blue-500 border flex items-center justify-center cursor-pointer transition-all duration-150 ${
-                isEyeHover ? "scale-[.95]" : ""
-              }`}
-              onMouseEnter={() => setIsEyeHover(true)}
-              onMouseLeave={() => setIsEyeHover(false)}
-            >
-              <FaEye
-                className={`text-[18px] max-lg:text-[17px] hover:text-blue-500 transition-all duration-150 ${
-                  isEyeHover ? "scale-[.95] text-blue-500" : ""
-                }`}
+        <div
+          className={`${
+            isImageLoading ? "opacity-0" : "opacity-100"
+          } w-full h-full flex items-center justify-between`}
+        >
+          <div className="w-auto h-full flex items-center justify-start gap-1">
+            <div className="w-[48px] max-lg:w-[46px] h-[48px] max-lg:h-[46px] rounded-md">
+              <Image
+                src={brandLogo}
+                alt="Brand Logo"
+                className="w-full h-full rounded-md"
               />
             </div>
-          </>
-        ) : null}
+            <h3 className="text-[17px] max-lg:text-[16px] font-bold font-OpenSans">
+              {brandName}
+            </h3>
+          </div>
+          <div
+            className={`w-[34px] max-lg:w-[30px] h-[34px] max-lg:h-[30px] bg-transparent rounded-[4px] border-slate-300 hover:border-blue-500 border flex items-center justify-center cursor-pointer transition-all duration-150 ${
+              isEyeHover ? "scale-[.95]" : ""
+            }`}
+            onMouseEnter={() => setIsEyeHover(true)}
+            onMouseLeave={() => setIsEyeHover(false)}
+          >
+            <FaEye
+              className={`text-[18px] max-lg:text-[17px] hover:text-blue-500 transition-all duration-150 ${
+                isEyeHover ? "scale-[.95] text-blue-500" : ""
+              }`}
+            />
+          </div>
+        </div>
+        {/* {!isImageLoading ? (
+    
+        ) : null} */}
       </div>
       <div className="relative w-full min-h-[60px] h-auto mt-1">
         {isImageLoading && (
